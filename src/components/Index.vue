2ccf67d87206504ed4956d415c795f8ddba10d7f
github username <github email address>
2019-11-01 18:40:20 -0600

    changed search string

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -49,17 +49,15 @@ export default {
       this.getRecipes()
     },
     getRecipes(){
-  
-      // axios.get("https://www.themealdb.com/api/json/v1/1/search.php?f=" + this.searchTerm)
-      axios.get("https://www.themealdb.com/api/json/v1/1/search.php?f=a")
-      .then(response => {console.log(response); return response})
+      axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + this.searchTerm)
       .then(response => response.data.meals.map(recipe => {
-
         this.ingredients = this.getIngredients(recipe)
         const ingredientList={ingredients : this.ingredients}
-        const recipes =  {...recipe, ...ingredientList}
-        this.recipeList = [...this.recipeList, recipes]
+        const recipeWithIngredients =  {...recipe, ...ingredientList}
+        console.log({ recipeWithIngredients })
+        return recipeWithIngredients
       }))
+      .then(recipesWithIngredients => {this.recipeList = recipesWithIngredients})
     }
   },
    
